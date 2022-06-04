084588ea334bbcff6820266fc34cdf352c61ee6a
Cairo Sales <cairoosf@gmail.com>
2022-06-04 12:16:53 -0300

    estrutura padrão de testes

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExempleTest extends TestCase{
+    public function test_example(){
+        return $this->assertTrue(true);
+    }
+}
